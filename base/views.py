6e5f7640f218f5eaa10b55b4898dd80024407b5b
Alem Dinarevic <alemdinarevic@gmail.com>
2024-03-29 00:52:35 +0100

    init commit

---
+++ base/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render, redirect
+from .models import Room
+from .forms import RoomForm
+
+# rooms = [
+#     {'id':1, 'name':'Lets learn python!'},
+#     {'id':2, 'name':'Lets learn django!'},
+#     {'id':3, 'name':'Lets learn frontend!'},
+# ]
+
+def home(request):
+    rooms = Room.objects.all()
+    context = { 'rooms': rooms }
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = { 'room': room }    
+    return render(request, 'base/room.html', context)
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        
+    context = { 'form': form }
+    return render(request, 'base/room_form.html', context)
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if (request.method == 'POST'):
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = { 'form': form }
+    return render(request, 'base/room_form.html', context)
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if (request.method == "POST"):
+        room.delete()
+        return redirect('home')
+    
+    context = { 'obj': room }
+    return render(request, 'base/delete.html', context)
